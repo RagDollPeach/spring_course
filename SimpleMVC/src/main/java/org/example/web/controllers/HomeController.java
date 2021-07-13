af2ed044967e86da9d54b88a6c0663145eb466a2
RagDollPeach <kudrad26@gmail.com>
2021-07-13 16:21:01 +0300

    FirstSpringMVC 1

--- SimpleMVC/src/main/java/org/example/web/controllers/HomeController.java
+++ SimpleMVC/src/main/java/org/example/web/controllers/HomeController.java
@@ -1,4 +1,4 @@
-package org.example.app.web.controllers;
+package org.example.web.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
