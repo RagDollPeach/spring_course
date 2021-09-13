006c43d06fb6e9ac4b9ca5be5e20f9425c8badb1
RagDollPeach <kudrad26@gmail.com>
2021-09-13 20:16:05 +0300

    BookShop

---
+++ hw3/MyBookShopApp/src/main/java/com/example/MyBookShopApp/data/BookService.java
@@ -0,0 +1,42 @@
+package com.example.MyBookShopApp.data;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Service;
+
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class BookService {
+
+    private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public BookService(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public List<Books> getBooksData() {
+        List<Books> books = jdbcTemplate.query("SELECT * FROM books", (ResultSet rs, int rowNum) -> {
+            Books book = new Books();
+            book.setId(rs.getInt("id"));
+            book.setAuthor(rs.getString("author"));
+            book.setTitle(rs.getString("title"));
+            book.setPriceOld(rs.getString("priceOld"));
+            book.setPrice(rs.getString("price"));
+            return book;
+        });
+        return new ArrayList<>(books);
+    }
+
+    public List<String> getAuthors() {
+        List<String> books = jdbcTemplate.query("SELECT author FROM books", (ResultSet rs, int rowNum) -> {
+            Books book = new Books();
+            book.setAuthor(rs.getString("author"));
+            return book.getAuthor();
+        });
+        return new ArrayList<>(books);
+    }
+}
