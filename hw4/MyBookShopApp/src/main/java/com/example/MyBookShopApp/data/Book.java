0d6050428ada533f5ee2ac8481f1dc45784bc90e
RagDollPeach <kudrad26@gmail.com>
2021-10-07 13:21:13 +0300

    test

---
+++ hw4/MyBookShopApp/src/main/java/com/example/MyBookShopApp/data/Book.java
@@ -0,0 +1,71 @@
+package com.example.MyBookShopApp.data;
+
+public class Book {
+
+    private Integer bookId;
+    private String authorFirstName;
+    private String authorLastName;
+    private String title;
+    private String priceOld;
+    private String price;
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "bookId=" + bookId +
+                ", authorFirstName='" + authorFirstName + '\'' +
+                ", authorLastName='" + authorLastName + '\'' +
+                ", title='" + title + '\'' +
+                ", priceOld='" + priceOld + '\'' +
+                ", price='" + price + '\'' +
+                '}';
+    }
+
+    public Integer getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(Integer bookId) {
+        this.bookId = bookId;
+    }
+
+    public String getAuthorFirstName() {
+        return authorFirstName;
+    }
+
+    public void setAuthorFirstName(String authorFirstName) {
+        this.authorFirstName = authorFirstName;
+    }
+
+    public String getAuthorLastName() {
+        return authorLastName;
+    }
+
+    public void setAuthorLastName(String authorLastName) {
+        this.authorLastName = authorLastName;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getPriceOld() {
+        return priceOld;
+    }
+
+    public void setPriceOld(String priceOld) {
+        this.priceOld = priceOld;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+}
